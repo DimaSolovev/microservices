eeeb3481a7641229e048e4a597ab8097022dcf72
Dima <soloveid87@mail.ru>
2022-05-05 14:34:11 +1000

    CustomerService

---
+++ customer/src/main/java/com/example/customer/CustomerController.java
@@ -0,0 +1,24 @@
+package com.example.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest){
+        log.info("new customer registration {}", customerRegistrationRequest);
+        customerService.registerCustomer(customerRegistrationRequest);
+    }
+}
