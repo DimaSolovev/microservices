13b234fe15adaef519c7ba77e0e2af2be0071ada
Dima <soloveid87@mail.ru>
2022-05-05 17:25:56 +1000

    fraud service and repo

--- customer/src/main/java/com/example/customer/CustomerController.java
+++ customer/src/main/java/com/example/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,13 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
+@AllArgsConstructor
 public class CustomerController {
 
     private final CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
 
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
--- customer/src/main/java/com/example/customer/CustomerService.java
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -1,15 +1,13 @@
 package com.example.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-
-    public CustomerService(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
