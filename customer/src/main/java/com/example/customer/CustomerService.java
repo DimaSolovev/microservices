439159add07347515672a4f8f947d836499d6ea5
Dima <soloveid87@mail.ru>
2022-05-06 13:48:39 +1000

    spring-cloud

--- customer/src/main/java/com/example/customer/CustomerService.java
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.example.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -15,6 +17,19 @@ public class CustomerService {
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .build();
-        customerRepository.save(customer);
+        //todo: check email
+        //todo: email taken
+        customerRepository.saveAndFlush(customer);
+        //todo: if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if(fraudCheckResponse.getIsFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
+
+        //todo: send notification
     }
 }
---
+++ customer/src/main/java/com/example/customer/FraudCheckResponse.java
@@ -0,0 +1,14 @@
+package com.example.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FraudCheckResponse {
+
+    private Boolean isFraudster;
+
+}
